76e86f1ae8a1a308d3bc5772f3ca9434a97beca2
jamal khan <139732458+khanjamal59@users.noreply.github.com>
2026-05-18 23:14:32 +0530

    Update chatbot.py

--- Prompts in langchain/chatbot.py
+++ Prompts in langchain/chatbot.py
@@ -1,1 +1,14 @@
 
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+load_dotenv()
+
+#model
+model=ChatGroq(model="llama-3.3-70b-versatile")
+while True:
+    user_input=input("You:")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Exiting the chatbot. Goodbye!")
+        break
+    model_response=model.invoke(user_input) 
+    print("Ai:",model_response.content)
