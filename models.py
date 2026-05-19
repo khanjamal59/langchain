6f0480a9a7b1f5cd2f18dbc6b3c36aae230e2f0f
jamal khan <139732458+khanjamal59@users.noreply.github.com>
2026-05-19 22:56:34 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv()#its for the env file
 model=ChatGroq(model="llama-3.3-70b-versatile",temperature=0.2) 
 result=model.invoke("what is the capital of nepal?")
 print(result.content)
