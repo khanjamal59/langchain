64e094ccda4322effcd2b6540b58a9c69ffd80b0
jamal khan <139732458+khanjamal59@users.noreply.github.com>
2026-05-20 23:13:47 +0530

    Update huggingface_chatmodel.py

--- Chat_model/huggingface_chatmodel.py
+++ Chat_model/huggingface_chatmodel.py
@@ -1,8 +1,9 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 load_dotenv()   
+usr_input=input("Enter your query")
 llm=HuggingFaceEndpoint(repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
                     task="text-generation")
 model=ChatHuggingFace(llm=llm)
-result=model.invoke("what is the capital of nepal?")
+result=model.invoke(usr_input)
 print(result.content)
