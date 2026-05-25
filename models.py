1af8088627b6c4f05fbfedea72ce731bf8d4cfe3
jamal khan <139732458+khanjamal59@users.noreply.github.com>
2026-05-25 23:42:26 +0530

    Refactor model invocation to use message structure
    Updated the model invocation to use a message array for interaction.

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
+from langchain_core.messages import SystemMessage, HumanMessage,AIMessage
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-load_dotenv()#its for the env file
-model=ChatGroq(model="llama-3.3-70b-versatile",temperature=0.2) 
-result=model.invoke("what is the capital of nepal?")
-print(result.content)
+load_dotenv()
+
+model=ChatGroq(model="llama-3.3-70b-versatile")
+
+messages=[
+    SystemMessage(content="You are a helpful assistant that writes short and sweet quotes for birthday cards."),
+    HumanMessage(content="write a short and sweet quotes for a birthday card")  
+]
+
+result=model.invoke(messages)
+messages.append(AIMessage(content=result.content))
+
+print("AI response:", result.content)
